9c20bfee6e88e90a381399b8f2c37407886eb4e8
Seth Torrence <slionel1850@gmail.com>
2023-06-03 14:10:12 -0400

    SavForDay

--- src/app/api/[club]/route.ts
+++ src/app/api/[club]/route.ts
@@ -1,3 +1,16 @@
-export const GET = () => {
-    
+import { prisma } from '@/utils/db'
+
+import { type NextRequest, NextResponse } from 'next/server'
+
+export async function GET(request: NextRequest, { params }: { params : { club: string }}) {
+    const trainers = await prisma.trainer.findMany({
+        where: {
+            club: Number(params.club)
+        }
+    })
+
+    NextResponse.json({
+        club: params.club,
+        trainers
+    })
 }
