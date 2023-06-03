2a81487b9a1190d4a5ea49f0a2da4c5375ee90bc
Seth Torrence <slionel1850@gmail.com>
2023-06-03 12:41:41 -0400

    FilterDividers

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -8,7 +8,7 @@ const Filter = ({ filterName }: { filterName: string }) => {
     const [active, setActive] = useState(false)
 
     return (
-        <span onClick={() => setActive(active => !active)} className={active? 'badge badge-lg badge-primary cursor-pointer' : 'badge badge-lg cursor-pointer'}>{capatalize(filterName)}</span>
+        <span onClick={() => setActive(active => !active)} className={active? 'badge badge-lg badge-primary cursor-pointer select-none' : 'select-none badge badge-lg cursor-pointer'}>{capatalize(filterName)}</span>
     )
 }
 
--- src/components/FilterContainer.tsx
+++ src/components/FilterContainer.tsx
@@ -5,6 +5,9 @@ const FilterContainer = () => {
         <div className='flex h-18 py-4 px-6 gap-2'>
             <Filter filterName='MORNING' />
             <Filter filterName='AFTERNOON' />
+            <div className="divider divider-horizontal">&</div>
+            <Filter filterName='CROSSFIT' />
+            <Filter filterName='STRENGTH' />
         </div>
     )
 }
