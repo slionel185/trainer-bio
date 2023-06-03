be9481b8fa8a0a5cc42bb939c0a2587874fe92cc
Seth Torrence <slionel1850@gmail.com>
2023-06-03 12:43:17 -0400

    No Text Divider

--- src/components/FilterContainer.tsx
+++ src/components/FilterContainer.tsx
@@ -5,7 +5,7 @@ const FilterContainer = () => {
         <div className='flex h-18 py-4 px-6 gap-2'>
             <Filter filterName='MORNING' />
             <Filter filterName='AFTERNOON' />
-            <div className="divider divider-horizontal">&</div>
+            <div className="divider divider-horizontal"></div>
             <Filter filterName='CROSSFIT' />
             <Filter filterName='STRENGTH' />
         </div>
